1a607529bc5d1f7ea0b66527f9b2d6c942a020f0
DenysBarkanov <denys@stakelogic.com>
2019-08-21 14:13:15 +0300

    test.js is done

--- test/specs/test.js
+++ test/specs/test.js
@@ -2,11 +2,15 @@ const assert = require('assert');
 
 describe('Website', function () {
     it('should be alive', function () {
-        
+        browser.url('/');
+        const img = $('img[src="http://ip-5236.sunline.net.ua:38015/images/logotype.png"]');
+        assert(img.isExisting(), 'Website should be opened, and logo displayed');
+        browser.pause(5000);
+    });
 
-        browser.url('/')
-        const img = $('img[src="http://ip-5236.sunline.net.ua:38015/images/logotype.png"]')
-        assert(img.isExisting(), 'Website should be opened, and logo displayed')
-        browser.pause(5000)
+    it('alive', function () {
+        browser.url('/');
+        const hm = $('.fa-home');
+        assert(hm.isExisting(), 'Website should be opened, and logo displayed');
     })
 })
