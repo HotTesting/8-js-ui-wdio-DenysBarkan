6c6d70ecb61617e66ef3f1b6d503ded16a26fd35
DenysBarkanov <denys@stakelogic.com>
2019-08-27 18:07:05 +0300

    test2

--- test/specs/test.js
+++ test/specs/test.js
@@ -5,12 +5,31 @@ describe('Website', function () {
         browser.url('/');
         const img = $('img[src="http://ip-5236.sunline.net.ua:38015/images/logotype.png"]');
         assert.exists(img, 'Website should be opened, and logo displayed');
-        browser.pause(3000);
+        //browser.pause(3000);
     });
 
     it('title', function () {
-        browser.url('/');
-        const title = browser.getTitle();
+        //browser.url('/');
+        let title = browser.getTitle();
         assert.equal(title, "Ducks Store | Online Store", "The Title is wrong");
+    });
+
+    it('cart', function () {
+        let cart = $('#cart');
+        let cartTitle = $(".details > .title").getText();
+        let cartImg = $(".image:nth-child(2)");
+        assert.exists(cart, "cart is not exist");
+        assert.exists(cartImg, "Cart hasn't the image");
+        assert.equal(cartTitle, "Shopping Cart", "The Title is not correct");
+    });
+
+    it("box slide", function () {
+        let slide = $("#box-slides > carousel-inner > item");
+        assert.exists(slide, "the slide box is not visiable");
+    });
+    it("corp logo", function () {
+        let corp = $("li img");
+       assert.exists(corp, "the corp logo is not shown");
     })
+
 })
