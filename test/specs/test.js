29f9d13fbdb6c0876e93ed33a44d381c2812101f
DenBarkan <45456152+DenBarkan@users.noreply.github.com>
2019-08-27 23:09:36 +0300

    test3

--- test/specs/test.js
+++ test/specs/test.js
@@ -1,4 +1,4 @@
-const {assert, expect} = require('chai');
+const {assert} = require('chai');
 
 describe('Website', function () {
     it('should be alive', function () {
@@ -7,13 +7,11 @@ describe('Website', function () {
         assert.exists(img, 'Website should be opened, and logo displayed');
         //browser.pause(3000);
     });
-
     it('title', function () {
         //browser.url('/');
         let title = browser.getTitle();
         assert.equal(title, "Ducks Store | Online Store", "The Title is wrong");
     });
-
     it('cart', function () {
         let cart = $('#cart');
         let cartTitle = $(".details > .title").getText();
@@ -22,7 +20,6 @@ describe('Website', function () {
         assert.exists(cartImg, "Cart hasn't the image");
         assert.equal(cartTitle, "Shopping Cart", "The Title is not correct");
     });
-
     it("box slide", function () {
         let slide = $("#box-slides > carousel-inner > item");
         assert.exists(slide, "the slide box is not visiable");
@@ -33,10 +30,24 @@ describe('Website', function () {
     });
     it("home btn", function () {
         let hb = $(".hidden-xs > a");
-        let ttl = (hb.getText());
-        let href = hb.getHTML();
-        //assert.equal(ttl, "Home");
+        let ttl = hb.getAttribute("title");
+        let href = hb.getAttribute("href");
+        assert.equal(ttl, "Home");
+        console.log(ttl);
         assert.equal(href, "http://ip-5236.sunline.net.ua:38015/");
+        console.log(href);
+    });
+    it("categories dropdown", function () {
+        let drdwn = $(".categories.dropdown");
+        let name = drdwn.getText();
+        assert.equal(name, "Categories", "The Title of dropdown list is incorrect");
+        $(".categories.dropdown > .dropdown-toggle").click();
+        let drmn = $(".categories > .dropdown-menu a");
+        console.log("drmn" + drmn);
+        let drList = $(drmn).getText();
+        let drLink = $(drmn).getAttribute("href");
+        assert.isTrue(drList);
+        assert.equal(drLink, "http://ip-5236.sunline.net.ua:38015/rubber-ducks-c-1/");
     })
 
 })
